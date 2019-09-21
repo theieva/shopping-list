32d876bb4372b153d8f7cd11bc83ac9a843afd7e
Ieva Urbaite <ieva@greyepk.com>
2019-09-21 15:44:03 -0400

    started shopping list js code

---
+++ index.js
@@ -0,0 +1,29 @@
+// 1. user enters text into textbox and clicks add item, add a box to the list
+// 2. check and uncheck items by clicking "check button", cross out checked item
+// 3. delete button would remove item boxes from list
+
+// create an event listener for submit button
+//function receiveInput(){
+$('#js-shopping-list-form').submit(function (event) {
+    $(event.currentTarget)
+
+// take what's in input and create new item
+   
+});
+//}
+
+// target the shopping-list-entry id -- this is the input box
+// copy 
+
+/* <li>
+<span class="shopping-item">TARGET_ITEM</span>
+<div class="shopping-item-controls">
+  <button class="shopping-item-toggle">
+    <span class="button-label">check</span>
+  </button>
+  <button class="shopping-item-delete">
+    <span class="button-label">delete</span>
+  </button>
+</div>
+</li> */
+
